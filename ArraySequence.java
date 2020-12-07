f86b06e2c991a466857806f8f8838fe075afdf21
David Chong <djchong24@gmail.com>
2020-12-07 13:47:11 -0500

    did all of the methods

--- ArraySequence.java
+++ ArraySequence.java
@@ -4,5 +4,27 @@ public class ArraySequence implements IntegerSequence{
   int []data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int [] other){
+      other = data;
+    }
+
+    public void reset(){
+        currentIndex = 0;
+    }
+
+    public int length(){
+        return data.length;
+    }
+
+    public boolean hasNext(){
+        if(currentIndex >= data.length - 1){
+            throw new NoSuchElementException("There are no more integers stored after " + data[currentIndex]);
+            return false;
+        }return true;
+    }
+
+    public int next(){
+        currentIndex ++;
+        return data[currentIndex - 1];
+    }
 }
